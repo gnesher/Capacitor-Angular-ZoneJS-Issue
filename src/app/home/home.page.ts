bea51afd16d9cb1fd36ed7ca39cd142c7d23c466
Guy Nesher <nesher.guy@gmail.com>
2020-09-02 08:38:11 +0300

    example of angular zones limitation with Capacitor plugins

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Network } = Plugins;
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public status = 'initial';
+
+  constructor(zone: NgZone) {
+    let handler = Network.addListener('networkStatusChange', (status) => {
+      console.log('status changed', status)
+      // this will show up on console, but won't change the ui
+      this.status = status.connected ? 'Online' : 'Offline'
+
+      // wrapping status within a zone will work
+      // zone.runTask(() => {
+      //   this.status = status.connected ? 'Online' : 'Offline'
+      // })
+      
+      console.log(this.status);
+    });
+  }
 
 }
